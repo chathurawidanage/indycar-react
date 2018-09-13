d8b2611baf9633f4482239f23133838f53f3bad6
Chathura Widanage <chathurawidanage@gmail.com>
2018-09-13 14:36:23 -0400

    Adding track component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, {Component} from 'react';
 import HeaderComponent from "./components/header/HeaderComponent";
 import "./App.css";
 import SpeedDataWrapper from "./components/speed/SpeedDataWrapper";
+import TrackComponent from "./components/track/TrackComponent";
 
 class App extends Component {
     render() {
         return (
             <div className="App">
                 <HeaderComponent/>
+                <TrackComponent/>
                 <SpeedDataWrapper/>
             </div>
         );
